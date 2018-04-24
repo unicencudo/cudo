76a8cad683c0e9263a93a664e4da3bafff62533c
Jidong Liu <jdliu@smu.edu.sg>
2018-04-24 21:47:58 +0800

    Add more files into project.

---
+++ config/default.js
@@ -0,0 +1,10 @@
+// config for mongodb
+module.exports = {
+    port: 3000,
+    session: {
+        secret: 'university',
+        key: 'university',
+        maxAge: 2592000000
+    },
+    mongodb: 'mongodb://localhost:27017/myblog'
+}
