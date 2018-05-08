53fdfbeefea3e82ba6ea17c5edd325e465fb0f45
Jidong Liu <jdliu@smu.edu.sg>
2018-05-08 14:57:16 +0800

    Basic API for order module: select/insert/update/delete.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var indexOrder = require('./routes/order');
+var orderRouter = require('./routes/order');
 var indexFlushDB = require('./routes/flushdb');
 
 var app = express();
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/order', indexOrder);
+app.use('/order', orderRouter);
 app.use('/flushdb', indexFlushDB);
 
 // catch 404 and forward to error handler
--- routes/order.js
+++ routes/order.js
@@ -12,8 +12,8 @@ router.get('/', function (req, res, next) {
 * */
 router.get('/all', function (req, res, next) {
     schema.order.findAll().then( orders => {
-        let myDate = new Date();
-        console.log(myDate.toLocaleString() + " GET: SELECT all orders");
+        let myDate = "[" + (new Date()).toLocaleString() + "]";
+        console.log(myDate + " GET: SELECT all orders");
         return orders;
     }).then(orders =>{
         res.json(orders);
@@ -22,55 +22,56 @@ router.get('/all', function (req, res, next) {
 });
 
 
-/* POST insert one order
+/* POST select/insert one order
 * */
 router.post('/insert', function (req, res, next) {
-    let myDate = new Date();
+    let myDate = "[" + (new Date()).toLocaleString() + "]";
     if (req.body && !Array.isArray(req.body)) {
         // OrderID maybe doesn't exist! Add check function in the further.
         if (req.body['orderID'])
         {
             schema.order.findOrCreate({where: {orderID: req.body['orderID']}, defaults: req.body})
                 .spread((order, created) => {
-                    console.log(myDate.toLocaleString() + " POST: INSERTION Created: " + created);
+                    console.log(myDate + " POST: INSERT Created: " + created);
                     res.json(req.body);
                 })
         }
         else {
             schema.order.create(req.body).then(() =>{
-                console.log(myDate.toLocaleString() + " POST: INSERTION")
+                console.log(myDate + " POST: INSERT")
             }).catch(()=>{
-                console.log(myDate.toLocaleString() + " POST: INSERTION ERROR")
+                console.log(myDate + " POST: INSERT ERROR")
             });
             res.json(req.body);
         }
     }
     else {
-        console.log(myDate.toLocaleString() + " POST: JSON ERROR");
+        console.log(myDate + " POST: JSON ERROR");
         res.status(400);
+        res.send(req.body)
     }
 });
 
 
-/* POST update order by orderID
+/* POST update/insert order by orderID
 * */
 router.post('/update', function (req, res, next) {
-    let myDate = new Date();
+    let myDate = "[" + (new Date()).toLocaleString() + "]";
     if (req.body && !Array.isArray(req.body)) {
         // OrderID maybe doesn't exist! Add check function in the further.
         if (req.body['orderID'])
         {
-            schema.order.findOrCreate({where: {orderID: req.body['orderID']}, defaults: req.body})
-                .spread((order, created) => {
-                    console.log(myDate.toLocaleString() + " POST: INSERTION Created: " + created);
+            schema.order.upsert(req.body, {where: {orderID: req.body['orderID']}})
+                .then((affected) =>{
+                    console.log(myDate + " POST: UPDATE orderID: " + req.body['orderID']);
                     res.json(req.body);
                 })
         }
         else {
             schema.order.create(req.body).then(() =>{
-                console.log(myDate.toLocaleString() + " POST: INSERTION")
+                console.log(myDate.toLocaleString() + " POST: UPDATE")
             }).catch(()=>{
-                console.log(myDate.toLocaleString() + " POST: INSERTION ERROR")
+                console.log(myDate.toLocaleString() + " POST: UPDATE ERROR")
             });
             res.json(req.body);
         }
@@ -78,9 +79,35 @@ router.post('/update', function (req, res, next) {
     else {
         console.log(myDate.toLocaleString() + " POST: JSON ERROR");
         res.status(400);
+        res.send(req.body)
     }
 });
 
+/* POST delete order by orderID
+* */
+router.post('/delete', function (req, res, next) {
+    let myDate = "[" + (new Date()).toLocaleString() + "]";
+    if (req.body && !Array.isArray(req.body)) {
+        // OrderID maybe doesn't exist! Add check function in the further.
+        if (req.body['orderID'])
+        {
+            schema.order.destroy({where: {orderID: req.body['orderID']}})
+                .then((affected) =>{
+                    console.log(myDate + " POST: DELETE orderID: " + req.body['orderID']);
+                    res.json(req.body);
+                })
+        }
+        else {
+            res.status(400);
+            res.json(req.body);
+        }
+    }
+    else {
+        console.log(myDate.toLocaleString() + " POST: JSON ERROR");
+        res.status(400);
+        res.send(req.body)
+    }
+});
 
 
 
